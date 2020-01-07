3650d04c8aee220e1817cc7d20061afb0bcb5c7e
peter <peterwangjiawei@gmail.com>
2020-01-06 21:12:01 -0500

    日期：1/5/2020 题目：【10】 1. Two Sum (2) 26. Remove Duplicates from Sorted Array(4) 27. Remove Element (1) 62. Unique Paths （2） 63. Unique Paths II 82. Remove Duplicates from Sorted List II 83. Remove Duplicates from Sorted List 103. Binary Tree Zigzag Level Order Traversal 947. Most Stones Removed with Same Row or Column 1249. Minimum Remove to Make Valid Parentheses 学习了:  DP, JAVA, ARray

--- src/Leetcode/Array/TwoSum_001.java
+++ src/Leetcode/Array/TwoSum_001.java
@@ -2,50 +2,17 @@ package Leetcode.Array;
 import java.util.*;
 
 public class TwoSum_001 {
-    public static int[] twosum(int[] nums, int target){
-        int[] ans = new int[2];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    ans[0] = i;
-                    ans[1] = j;
-                }
+    public static int[] twosum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[1] = i;
+                res[0] = map.get(target - nums[i]);
+                return res;
             }
+            map.put(nums[i], i);
         }
-        return ans;
-    }
-
-    public static int[] twoSum(int[] numbers, int target) {
-
-        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
-        for(int i = 0; i < numbers.length; i++){
-            Integer diff = (target - numbers[i]);
-            if(hash.containsKey(diff)){
-                int toReturn[] = {hash.get(diff)+1, i+1};
-                return toReturn;
-            }
-            hash.put(numbers[i], i);
-        }
-        return null;
-    }
-    public static void main(String[] args){
-        int[] nums ={2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twosum(nums, target)));
-
-        for(int i = 0; i <= 3; i++){
-            char ch = 'a';
-            char print = ch;
-            for (int j =  0; j <= i; j++){
-                System.out.print((print++));
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < 3; i++)
-            System.out.println("cute");
-            System.out.println("kk");
-        int i = 0;
-        if (i == 0)
-            System.out.println("i == 0");
+        return res;
     }
 }
