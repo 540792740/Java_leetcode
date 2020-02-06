aeb7a449b33c7ae94589487d6ac78ca98220670c
peter <peterwangjiawei@gmail.com>
2020-02-06 11:40:45 -0500

    Thread

---
+++ src/Summit_train/Day7_multiThread.java
@@ -0,0 +1,27 @@
+package Summit_train;
+
+class test{}
+class mythread extends Thread{
+    // Overwrite run function
+    public void run(){System.out.println("Thread overwrite run");}
+}
+
+class mythread1 extends test implements Runnable{
+    // Overwrite run function
+    public void run(){ System.out.println("Thread1 overwrite run");}
+}
+
+public class Day7_multiThread {
+    public static void main(String[] args){
+        System.out.println("First line");
+        // Run Thread by using start();
+        mythread m = new mythread();
+        m.start();
+        System.out.println("last line");
+
+//        Interface Thread
+        mythread1 m1 = new mythread1();
+        Thread t = new Thread(m1);
+        t.start();
+    }
+}
